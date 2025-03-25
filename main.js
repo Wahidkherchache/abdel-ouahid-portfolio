010b0daa9bacea35f4da6b186823731f96609b87
Wahidkherchache <wahidkherchache@gmail.com>
2025-03-25 22:33:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 let year = document.getElementById("new");
 year.textContent = new Date().getFullYear();
+const web1 = document.getElementById("tamplate1");
+const web2 = document.getElementById("tamplate2");
+        function gotosite(url){
+            window.open(url ,"_blank") ;
+        }
+web1.onclick = function() {
+    gotosite("https://wahidkherchache.github.io/web-two/");
+}
+web2.onclick = function() {
+    gotosite("https://wahidkherchache.github.io/ouahid-tamplate/")
+}
